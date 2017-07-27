6e3b6dbe2d5d3c44a771a158a9bea2d3e3a074f0
Uselessness <andrew13131336@gmail.com>
2017-07-27 23:23:03 +0400

    toasty

--- app/src/main/java/com/example/uselesss/myapplication/MainActivity.java
+++ app/src/main/java/com/example/uselesss/myapplication/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.uselesss.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,31 +18,41 @@ public class MainActivity extends AppCompatActivity {
     TextView summ;
     Button plus;
     Button minus;
-    Piece piece;
-    int s;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        s = 0;
+        pieces = new ArrayList<>();
         plus = (Button) findViewById(R.id.plus);
         minus = (Button) findViewById(R.id.minus);
         summ = (TextView) findViewById(R.id.summ);
-        piece = new Piece(2.3);
-//        pieces.add(new Piece(1.1));
-        summ.setText("" + piece.getSumm());
-        plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                piece.add();
-                summ.setText("" + piece.getSumm());
-            }
-        });
+        Piece piece = new Piece(2.35);
+        pieces.add(piece);
+        summ.setText(getSumm());
     }
 
-    public void onClick(View view) {
-        piece.remove();
-        summ.setText("" + piece.getSumm());
+    public void showToastSumm(){
+        Toast toast = Toast.makeText(MainActivity.this, getSumm(), Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    public String getSumm(){
+        return String.valueOf(pieces.get(0).getSumm());
+    }
+
+    public void  plusButtonClick(View view){
+        Log.e("E", "PLUS pressed");
+        pieces.get(0).add();
+        summ.setText(getSumm());
+        showToastSumm();
+    }
+
+    public void  minusButtonClick(View view){
+        pieces.get(0).remove();
+        summ.setText(getSumm());
+        showToastSumm();
     }
 }
